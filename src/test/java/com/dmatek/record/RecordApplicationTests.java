6dc46ee2ac53a32970ae95ea15eef3627fe94165
root <root>
2020-06-03 17:54:58 +0800

    first commit

---
+++ src/test/java/com/dmatek/record/RecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmatek.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
